163cd97971dc0503c8779f785f39b792486b9bf9
zena-git <chienlai2k3@hotmail.com>
2023-10-26 15:10:47 +0700

    update

--- admin/angular.js
+++ admin/angular.js
@@ -7,102 +7,97 @@ app.config(function ($routeProvider,$locationProvider) {
             templateUrl: '../admin/home.html',
             
         }) 
-        .when('/page/footer', {
+        .when('/footer', {
             templateUrl: '../admin/footer.html',
             
         }) 
-        .when('/page/home', {
-            templateUrl: '../admin/page/home.html',
-
-        })
-        .when('/page/ve-chung-toi', {
+         .when('/ve-chung-toi', {
             templateUrl: '../admin/page/ve-chung-toi.html',
             
         })
-
-         .when('/page/ve-chung-toi', {
-            templateUrl: '../admin/page/ve-chung-toi.html',
-            
-        })
-         .when('/page/chinh-sach-phan-phoi', {
+         .when('/chinh-sach-phan-phoi', {
             templateUrl: '../admin/page/chinh-sach-phan-phoi.html',
             
         })
-         .when('/page/com-ngon', {
+         .when('/com-ngon', {
             templateUrl: '../admin/page/com-ngon.html',
             
         })
-         .when('/page/dac-san', {
+         .when('/dac-san', {
             templateUrl: '../admin/page/dac-san.html',
             
         })
-         .when('/page/dang-ky-dai-hop-tac', {
+         .when('/dang-ky-dai-hop-tac', {
             templateUrl: '../admin/page/dang-ky-dai-hop-tac.html',
             
         })
-         .when('/page/dang-ky-dai-ly', {
+         .when('/dang-ky-dai-ly', {
             templateUrl: '../admin/page/dang-ky-dai-ly.html',
             
         })
 
-         .when('/page/gao-nuong', {
+         .when('/gao-nuong', {
             templateUrl: '../admin/page/gao-nuong.html',
             
         })
-         .when('/page/goi-hop-tac', {
+         .when('/goi-hop-tac', {
             templateUrl: '../admin/page/goi-hop-tac.html',
             
         })
-         .when('/page/he-thong-cua-hang', {
+         .when('/he-thong-cua-hang', {
             templateUrl: '../admin/page/he-thong-cua-hang.html',
             
         })
-         .when('/page/lien-he', {
+         .when('/lien-he', {
             templateUrl: '../admin/page/lien-he.html',
             
         })
-         .when('/page/loi-the-mo-hinh', {
+         .when('/loi-the-mo-hinh', {
             templateUrl: '../admin/page/loi-the-mo-hinh.html',
             
         })
-         .when('/page/quy-trinh-hop-tac', {
+         .when('/quy-trinh-hop-tac', {
             templateUrl: '../admin/page/quy-trinh-hop-tac.html',
             
         })
-         .when('/page/mot-so-hinh-anh', {
+         .when('/mot-so-hinh-anh', {
             templateUrl: '../admin/page/mot-so-hinh-anh.html',
             
         })
-         .when('/page/san-pham', {
-            templateUrl: '../admin/page/san-pham.html',
+         .when('/san-pham', {
+            templateUrl: '../admin/view/product/product.html',
             controller: 'productController'
         })
 
-        .when('/page/san-pham/new', {
-            templateUrl: '../admin/page/san-pham-new.html',
+        .when('/san-pham/new', {
+            templateUrl: '../admin/view/product/product-new.html',
            
         })
 
-        .when('/page/san-pham/:id', {
-            templateUrl: '../admin/page/san-pham-edit.html',
+        .when('/san-pham/:id', {
+            templateUrl: '../admin/view/product/product-edit.html',
            
         })
 
-         .when('/page/tin-tuc', {
+         .when('/tin-tuc', {
             templateUrl: '../admin/page/tin-tuc.html',
             
         })
-        .when('/page/tin-tuc/new', {
+        .when('/tin-tuc/new', {
             templateUrl: '../admin/page/tin-tuc-new.html',
             controller: 'tintucNewController'
         })
-        .when('/page/tin-tuc/:url', {
+        .when('/tin-tuc/:url', {
             templateUrl: '../admin/page/tin-tuc-new.html',
             controller: 'tintucDetailController'
             
         })
-         .when('/page/vi-sao-chon-chung-toi', {
+         .when('/vi-sao-chon-chung-toi', {
             templateUrl: '../admin/page/vi-sao-chon-chung-toi.html',
+            
+        })
+        .when('/setting-pages', {
+            templateUrl: '../admin/blankpage.html',
             
         })
         .otherwise({
